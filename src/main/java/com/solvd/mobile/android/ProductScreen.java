f7496e056a5ed1479bed4eff2d018e1c214c1fd0
Erika Rodriguez <rodriguez.eri92@gmail.com>
2023-04-19 09:33:18 -0300

    testSubmitReviewOnProduct on Android

--- src/main/java/com/solvd/mobile/android/ProductScreen.java
+++ src/main/java/com/solvd/mobile/android/ProductScreen.java
@@ -13,8 +13,7 @@ public class ProductScreen extends ProductScreenBase {
     @ExtendedFindBy(text = "Add to cart")
     private ExtendedWebElement addToCartBtn;
 
-    //@FindBy(xpath = "//*[resource-id='com.saucelabs.mydemoapp.android:id/start1IV']")
-    @FindBy(xpath = "//android.widget.FrameLayout[@content-desc=\"Container for fragments\"]/android.widget.ScrollView/android.view.ViewGroup/android.widget.LinearLayout[1]/android.view.ViewGroup/android.widget.LinearLayout/android.widget.ImageView[2]")
+    @FindBy(id = "com.saucelabs.mydemoapp.android:id/start1IV")
     private ExtendedWebElement oneStarBtn;
     public ProductScreen(WebDriver driver) {
         super(driver);
